1b3f2730fecb0a2d17b61c07cc7da962106ac55f
Moisés Alemám <hxrox@hotmail.com>
2016-04-10 23:11:52 -0600

    Modificación migrations init

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -6,17 +6,74 @@ class m130524_201442_init extends Migration
 {
     public function up()
     {
+        //Opciones para la creación de tablas
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
+        ##################
+        ################## Creación de tablas
+        ##################
+
+        //Crear tabla de acciones
+        $this->createTable('{{%acciones}}',[
+        ], $tableOptions);
+
+        //Crear tabla de bancos
+        $this->createTable('{{%bancos}}',[
+        ], $tableOptions);
+
+        //Crear tabla de flores
+        $this->createTable('{{%flores}}',[
+        ], $tableOptions);
+
+        //Crear tabla de comisiones
+        $this->createTable('{{%comisiones}}',[
+        ], $tableOptions);
+
+        //Crear tabla de solicitudes_depositos
+        $this->createTable('{{%solicitudes_depositos}}',[
+        ], $tableOptions);
+
+        //Crear tabla de modulos
+        $this->createTable('{{%modulos}}',[
+        ], $tableOptions);
+
+        //Crear tabla de notificaciones
+        $this->createTable('{{%notificaciones}}',[
+        ], $tableOptions);
+
+        //Crear tabla de invitaciones
+        $this->createTable('{{%invitaciones}}',[
+        ], $tableOptions);
+
+        //Crear tabla de ordenes_pagos
+        $this->createTable('{{%ordenes_pagos}}',[
+        ], $tableOptions);
+
+        //Crear tabla de permisos
+        $this->createTable('{{%permisos}}',[
+        ], $tableOptions);
+
+        //Crear tabla de planes
+        $this->createTable('{{%planes}}',[
+        ], $tableOptions);
+
+        //Crear tabla de roles
+        $this->createTable('{{%roles}}',[
+        ], $tableOptions);
+
+        //Crear tabla de ubicaciones
+        $this->createTable('{{%ubicaciones}}',[
+        ], $tableOptions);
+
+        //Crear tabla de usuarios
         $this->createTable('{{%usuarios}}', [
-            'id' => $this->primaryKey(),
-            'email' => $this->string()->notNull()->unique(),
-            'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
+            'id' => $this->primaryKey(), //Llave primaria
+            'email' => $this->string()->notNull()->unique(), //email unico para acceso
+            'auth_key' => $this->string(32)->notNull(), //
+            'password_hash' => $this->string()->notNull(), //Contraseña encriptada 
             'password_reset_token' => $this->string()->unique(),
             'nombres' => $this->string()->notNull(),
             'apellidos' => $this->string()->notNull(),
@@ -26,8 +83,10 @@ class m130524_201442_init extends Migration
             'domicilio' => $this->string()->notNull(),
             'cuenta_bancaria' => $this->bigInteger()->notNull(),
             'balance' => $this->decimal(10,2)->defaultValue(0),
-            'id_banco' => $this->integer()->notNull(),
-
+            'id_banco' => $this->integer()->notNull(), //Llave FORANEA de la tabla de bancos
+            'id_ubicacion' => $this->integer()->notNull(), //Llave FORANEA de la tabla de ubicaciones
+            'token' => $this->string()->unique(), //token para la validación de información por email
+            
             'fecha_creado' => $this->dateTime()->defaultValue('NOW()'),
             'fecha_modificado' => $this->dateTime(),
             'fecha_eliminado' => $this->dateTime(),
@@ -36,6 +95,18 @@ class m130524_201442_init extends Migration
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        //Crear tabla de usuarios_accesos
+        $this->createTable('{{%usuarios_accesos}}',[
+        ], $tableOptions);
+
+        ##################
+        ################## Creación de indices
+        ##################
+
+        ##################
+        ################## Creación de llaves foraneas
+        ##################
 
         //Agregar llave foranea
         $this->addForeignKey(
@@ -50,6 +121,23 @@ class m130524_201442_init extends Migration
 
     public function down()
     {
+        ##################
+        ################## Eliminación de tablas
+        ##################
+        $this->dropTable('{{%acciones}}');
+        $this->dropTable('{{%bancos}}');
+        $this->dropTable('{{%flores}}');
+        $this->dropTable('{{%comisiones}}');
+        $this->dropTable('{{%solicitudes_depositos}}');
+        $this->dropTable('{{%modulos}}');
+        $this->dropTable('{{%notificaciones}}');
+        $this->dropTable('{{%invitaciones}}');
+        $this->dropTable('{{%ordenes_pagos}}');
+        $this->dropTable('{{%permisos}}');
+        $this->dropTable('{{%planes}}');
+        $this->dropTable('{{%roles}}');
+        $this->dropTable('{{%ubicaciones}}');
         $this->dropTable('{{%usuarios}}');
+        $this->dropTable('{{%usuarios_accesos}}');
     }
 }
